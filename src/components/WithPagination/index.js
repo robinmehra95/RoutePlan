b575e8f42ead53d133028f3d2d3f498f3b0762c2
yetinonline <yatendra.bhardwaj@tothenew.com>
2019-08-25 03:15:52 +0530

    Filter logic added

--- src/components/WithPagination/index.js
+++ src/components/WithPagination/index.js
@@ -3,18 +3,11 @@ import './style.css';
 import ClatexStationRow from '../ClatexStationRow';
 import img3 from './../../img/icons_select_arrow@1x.svg';
 
-const exampledata = [
-    {a:1},{b:1},{c:1},{d:1},{e:1},
-    {f:1},{g:1},{h:1},{i:1},{j:1},{k:1},
-    {l:1},{m:1},{n:1},{o:1},{p:1},{q:1},
-    {r:1},{s:1},{t:1},{u:1},{v:1},{w:1},
-    {x:1},{y:1},{z:1}
-  ]
 
 class WithPagination extends React.Component {
   constructor(props) {
     super(props); 
-    this.totalRecords = this.props.allStations ? this.props.allStations : exampledata;
+    this.totalRecords = this.props.allStations 
     this.totalPages = Math.ceil(this.totalRecords.length / 4); 
     let slicedData = this.totalRecords.slice(0, 4);
     this.state = {
